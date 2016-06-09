df6183272adecc7d66eac5789659b49d8be5e91a
Louis-Guillaume Gagnon <louis.guillaume.gagnon@cern.ch>
2016-06-09 09:55:54 -0400

    fix submission scripts

--- launch.sh
+++ launch.sh
@@ -2,23 +2,20 @@
 . $ATLAS_LOCAL_ROOT_BASE/packageSetups/localSetup.sh 'asetup AtlasProduction,19.2.5.5'
 
 
-datadir=/lcg/storage15/atlas/gagnon/work/generate_gtb
+repo=/home/zp/gagnon/dev/gtb
 
-wdir=run_$(date --iso=seconds)
-mkdir $wdir
-cd $wdir
+mkdir $JOBNAME
+cd $JOBNAME
 
-cp $datadir/MC15.000000.MGPy8EG_A14N23LO_GG_tbn1_1800_5000_100.py .
-cp $datadir/MadGraphControl_SimplifiedModel_GG_tbn1.py .
-cp $datadir/param_card.SM.GG.tbn1.dat .
+git clone $repo .
 
 export ATHENA_PROC_NUMBER=$NJOBS
 Generate_tf.py --ecmEnergy=13000. \
 	       --runNumber=000000 \
 	       --firstEvent=1 \
-	       --maxEvents=-1 \
-	       --randomSeed=8461 \
-	       --jobConfig=MC15.000000.MGPy8EG_A14N23LO_GG_tbn1_1800_5000_100.py \
-	       --outputEVNTFile=gtb_test.pool.root
+	       --maxEvents=10000 \
+	       --randomSeed=750 \
+	       --jobConfig=MC15.000000.MGPy8EG_A14N23LO_GG_${NAME}_1800_5000_100.py \
+	       --outputEVNTFile=${NAME}_test.pool.root
 
 
--- submit.sh
+++
@@ -1,5 +0,0 @@
-qsub -N generate_gtb \
-     -d $PWD \
-     -j oe \
-     -l nodes=1:ppn=${NJOBS-20},nice=0 \
-     launch.sh
