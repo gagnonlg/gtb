2c97f654258710fc81b83c91a56770a47348df7b
Louis-Guillaume Gagnon <louis.guillaume.gagnon@cern.ch>
2016-07-25 08:09:22 -0400

    update batch submission scripts

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 . $ATLAS_LOCAL_ROOT_BASE/packageSetups/localSetup.sh 'asetup AtlasProduction,19.2.5.5'
 
 
-repo=/home/zp/gagnon/dev/gtb
+repo="/lcg/storage15/atlas/gagnon/work/2016-07-13_submit_gtb_100k/gtb"
 
 mkdir $JOBNAME
 cd $JOBNAME
@@ -13,8 +13,8 @@ export ATHENA_PROC_NUMBER=$NJOBS
 Generate_tf.py --ecmEnergy=13000. \
 	       --runNumber=000000 \
 	       --firstEvent=1 \
-	       --maxEvents=10000 \
-	       --randomSeed=750 \
+	       --maxEvents=100000 \
+	       --randomSeed=9841 \
 	       --jobConfig=MC15.000000.MGPy8EG_A14N23LO_GG_${NAME}_1800_5000_100.py \
 	       --outputEVNTFile=${NAME}_test.pool.root
 
