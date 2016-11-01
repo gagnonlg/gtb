acb8547325b81ad0f4d9b42f5ee86eb037024c3b
Louis-Guillaume Gagnon <louis.guillaume.gagnon@cern.ch>
2016-11-01 17:49:00 -0400

    fix maxEvents

--- launch_gtb.sh
+++ launch_gtb.sh
@@ -7,11 +7,17 @@ cd ${PBS_JOBID}_${PBS_JOBNAME}
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 git clone $SCRIPT_DIR .
 
+if [ "$NAME" = "2top" ]; then
+    maxEvents=10000
+else
+    maxEvents=6000
+fi
+
 export ATHENA_PROC_NUMBER=$NJOBS
 Generate_tf.py --ecmEnergy=13000. \
 	       --runNumber=000000 \
 	       --firstEvent=1 \
-	       --maxEvents=10000 \
+	       --maxEvents=$maxEvents \
 	       --randomSeed=12507 \
 	       --jobConfig=MC15.000000.MGPy8EG_A14N23LO_GG_tb_${MASSES}_${NAME}.py \
 	       --outputEVNTFile=gtb_${NAME}_${MASSES}_test.pool.root
