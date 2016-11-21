38cf0a06ce74c9fb6745ea568253405e06706e53
Louis-Guillaume Gagnon <louis.guillaume.gagnon@cern.ch>
2016-11-21 10:43:45 -0500

    duplicate derivation script in right place

---
+++ submit-derivation.sh
@@ -0,0 +1,12 @@
+set -u
+set -e
+path=$(readlink -f $1)
+jobname=derivation_$(basename $path .pool.root)_$(date --iso=seconds)
+
+cat <<EOF | qsub -N $jobname -d /lcg/storage15/atlas/gagnon/work -joe -l nice=0
+mkdir $jobname
+cd $jobname
+. /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh
+. $ATLAS_LOCAL_ROOT_BASE/packageSetups/localSetup.sh 'asetup 20.7.5.41,AtlasDerivation,gcc49,here'
+Reco_tf.py --inputEVNTFile $path --outputDAODFile $(basename $path | sed 's/.pool.root/TRUTH3.root/') --reductionConf TRUTH3
+EOF
